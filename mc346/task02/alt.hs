e880054211a3ec20bab7372b2848ddc1d17b70cc
Giovanne <57878035+giolucasd@users.noreply.github.com>
2023-02-08 16:26:38 -0300

    Create alt.hs

---
+++ mc346/task02/alt.hs
@@ -0,0 +1,8 @@
+insertionSort :: Ord a => [a] -> [a]
+insertionSort [] = []
+insertionSort [x] = [x]
+insertionSort (x:xs) = insert $ insertionSort xs
+    where insert [] = [x]
+          insert (y:ys)
+              | x < y = x : y : ys
+              | otherwise = y : insert ys
